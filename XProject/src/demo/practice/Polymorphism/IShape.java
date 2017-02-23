772302d93c8a3c7b089384bf85df7d34fd26ee19
jotaiwan <jotaiwan@gmail.com>
2017-02-23 20:32:17 +1100

    refactory the folder instruction

--- XProject/src/demo/practice/Polymorphism/IShape.java
+++
@@ -1,9 +0,0 @@
-package demo.practice.Polymorphism;
-
-/**
- * Created by jchen on 19/02/17.
- */
-public interface IShape {
-    public double area();
-    public double volume();
-}
---
+++ XProject/src/practice/Polymorphism/Cube.java
@@ -0,0 +1,14 @@
+package practice.Polymorphism;
+
+/**
+ * Created by jchen on 19/02/17.
+ */
+public class Cube implements IShape {
+    int radius = 10;
+    public double area() {
+        return (Math.PI * radius * radius);
+    }
+    public double volume() {
+        return 0;
+    }
+}
