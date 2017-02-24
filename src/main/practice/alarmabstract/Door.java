03748e520052f2a49d4e99fafe837a9faa71465b
jotaiwan <jotaiwan@gmail.com>
2017-02-25 10:08:42 +1100

    refactory code 3

---
+++ src/main/practice/alarmabstract/Door.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice.alarmabstract;
+
+/**
+ *
+ * @author jotaiwan
+ */
+public abstract class Door {
+    public abstract void open();
+    public abstract void close();
+}
---
+++ src/main/practice/polymorphism/IShape.java
@@ -0,0 +1,9 @@
+package practice.polymorphism;
+
+/**
+ * Created by jchen on 19/02/17.
+ */
+public interface IShape {
+    public double area();
+    public double volume();
+}
