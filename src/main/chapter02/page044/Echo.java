5e5d413059bc2c2f591c285feec1508bd098c241
jotaiwan <jotaiwan@gmail.com>
2017-02-26 19:40:37 +1100

    chapter02-page-044

---
+++ src/main/chapter02/page044/Echo.java
@@ -0,0 +1,11 @@
+package chapter02.page044;
+
+/**
+ * Created by jotaiwan on 26/02/2017.
+ */
+public class Echo {
+    int count = 0;
+    void hello() {
+        System.out.println("helloooooo...");
+    }
+}
